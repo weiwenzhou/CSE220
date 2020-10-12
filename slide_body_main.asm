c2d5ba48cbdf3e3b42965ad3ca12e507d87f8b59
wzhou2 <wzhou2@stuy.edu>
2020-10-12 17:47:40 -0400

    simple testing

--- slide_body_main.asm
+++ slide_body_main.asm
@@ -1,6 +1,6 @@
 .data
-head_row_delta: .byte 0
-head_col_delta: .byte -1
+head_row_delta: .byte -1
+head_col_delta: .byte 0
 apples: .byte 1 7 3 2 1 4 4 3
 apples_length: .word 4
 .align 2
@@ -13,24 +13,38 @@ state:
 # Game grid:
 .asciiz "....................##......................#..a.....#....#..1234..#..........56...E......##.7..CD.........89AB."
 
+.macro newline
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+.end_macro
+
 .text
 .globl main
 main:
-la $a0, state
-lb $a1, head_row_delta
-lb $a2, head_col_delta
-la $a3, apples
-addi $sp, $sp, -4
-lw $t0, apples_length
-sw $t0, 0($sp)
-li $t0, 7918273    # putting some random garbage in $t0
-jal slide_body
-addi $sp, $sp, 4
+	la $a0, state
+	lb $a1, head_row_delta
+	lb $a2, head_col_delta
+	la $a3, apples
+	addi $sp, $sp, -4
+	lw $t0, apples_length
+	sw $t0, 0($sp)
+	li $t0, 7918273    # putting some random garbage in $t0
+	jal slide_body
+	addi $sp, $sp, 4
 
-
-# You must write your own code here to check the correctness of the function implementation.
-
-li $v0, 10
-syscall
+	# You must write your own code here to check the correctness of the function implementation.
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	newline # 6
+	
+	la $s0, state 
+	addi $a0, $s0, 5 # to get to string
+	li $v0, 4 
+	syscall # 10
+	
+	li $v0, 10
+	syscall
 
 .include "hwk3.asm"
