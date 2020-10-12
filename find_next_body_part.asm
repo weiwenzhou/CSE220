1433f85e0a36e1c848aa6ccc30a401538fc4b2cf
wzhou2 <wzhou2@stuy.edu>
2020-10-12 16:02:32 -0400

    quick testing for find_next_body_part

--- find_next_body_part.asm
+++ find_next_body_part.asm
@@ -1,7 +1,7 @@
 .data
 row: .byte 3
 col: .byte 5
-target_part: .byte '4'
+target_part: .byte 9
 .align 2
 state:
 .byte 5  # num_rows
@@ -12,19 +12,33 @@ state:
 # Game grid:
 .asciiz ".............a.#.1..#......#.2..#......#.3..#........4567..."
 
+.macro newline
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+.end_macro
+
 .text
 .globl main
 main:
-la $a0, state
-lb $a1, row
-lb $a2, col
-lbu $a3, target_part
-jal find_next_body_part
+	la $a0, state
+	lb $a1, row
+	lb $a2, col
+	lbu $a3, target_part
+	jal find_next_body_part
 
 
-# You must write your own code here to check the correctness of the function implementation.
-
-li $v0, 10
-syscall
+	# You must write your own code here to check the correctness of the function implementation.
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	newline # 6
+	
+	move $a0, $v1
+	li $v0, 1
+	syscall # 9
+	
+	li $v0, 10
+	syscall
 
 .include "hwk3.asm"
