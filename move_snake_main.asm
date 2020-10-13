dc1c46601f4bd9290945bce02bd7afc7fb019ab3
wzhou2 <wzhou2@stuy.edu>
2020-10-12 22:27:37 -0400

    quick testing for move_snake

--- move_snake_main.asm
+++ move_snake_main.asm
@@ -12,19 +12,38 @@ state:
 # Game grid:
 .asciiz "....................##......................#..a.....#....#..1234..#..........56...E......##.7..CD.........89AB."
 
+.macro newline
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+.end_macro
 
 .text
 .globl main
 main:
-la $a0, state
-lbu $a1, direction
-la $a2, apples
-lw $a3, apples_length
-jal move_snake
+	la $a0, state
+	lbu $a1, direction
+	la $a2, apples
+	lw $a3, apples_length
+	jal move_snake
 
-# You must write your own code here to check the correctness of the function implementation.
-
-li $v0, 10
-syscall
+	# You must write your own code here to check the correctness of the function implementation.
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	newline # 6
+	
+	move $a0, $v1
+	li $v0, 1
+	syscall
+	newline # 12
+	
+	la $s0, state 
+	addi $a0, $s0, 5 # to get to string
+	li $v0, 4 
+	syscall # 16
+	
+	li $v0, 10
+	syscall
 
 .include "hwk3.asm"
