74021aba4dcfcf2b95b50742533ea2bd272e5735
wzhou2 <wzhou2@stuy.edu>
2020-10-26 14:48:07 -0400

    wrote strcmp and light testing

--- hwk4.asm
+++ hwk4.asm
@@ -30,7 +30,58 @@ memcpy: # int memcpy(byte[] dest, byte[] src, int n)
     memcpy_done:
     jr $ra
 
-strcmp:
+strcmp: # int strcmp(string str1, string str2)
+    # a0 -> str1 null terminated
+    # a1 -> str2 null terminated
+    # => v0 str1[n] - str2[n] if difference else 0 if str1 === str2 
+        # else str1.length if str1 != "" && str2 == "" 
+        # else -str2.length if str1 == "" && str2 != ""
+
+    # check if str1 == ""
+    lbu $t0, 0($a0)
+    beqz $t0, strcmp_empty_case
+    lbu $t0, 0($a1)
+    beqz $t0, strcmp_empty_case
+
+    li $t9, 0 # tracker for non_empty
+    j strcmp_not_empty
+    strcmp_empty_case: # Note: includes both empty since they would be equal -> 0
+        li $t8, 0 # length of str1
+        li $t9, 0 # length of str2
+        strcmp_find_str1: # find length of str1
+            add $t0, $a0, $t8
+            lbu $t0, 0($t0)
+            beqz $t0, strcmp_find_str2 
+            addi $t8, $t8, 1 # increment
+            j strcmp_find_str1
+            
+        strcmp_find_str2: # find length of str2
+            add $t0, $a1, $t9
+            lbu $t0, 0($t0)
+            beqz $t0, strcmp_take_diff
+            addi $t9, $t9, 1 # increment
+            j strcmp_find_str2
+
+        strcmp_take_diff: 
+            sub $v0, $t8, $t9
+            jr $ra
+
+    # !(one str empty) 
+    strcmp_not_empty:
+        add $t0, $a0, $t9
+        add $t1, $a1, $t9
+        lbu $t0, 0($t0)
+        lbu $t1, 0($t1)
+        addi $t9, $t9, 1 # increment
+        
+        sne $v0, $t0, $t1 # 0 if t0 == t1 else 1
+        sub $t8, $t0, $t1 # 1 -> t0 != t1 -> v0 = t0 - t1
+        blez $t0, strcmp_done 
+        blez $t1, strcmp_done
+        beqz $v0, strcmp_not_empty # 0 -> check next bit
+        
+    strcmp_done:
+        move $v0, $t8
     jr $ra
 
 initialize_hashtable:
--- strcmp_main.asm
+++ strcmp_main.asm
@@ -5,12 +5,16 @@ str2: .asciiz "ABCGG"
 .text
 .globl main
 main:
-la $a0,  str1
-la $a1,  str2
-jal strcmp
+	la $a0,  str1
+	la $a1,  str2
+	jal strcmp
 
-# Write code to check the correctness of your code!
-li $v0, 10
-syscall
+	# Write code to check the correctness of your code!
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall
 
 .include "hwk4.asm"
