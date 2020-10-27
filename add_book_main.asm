a7ed856f3f906cab9d9cfba7ba2f5a1e36a23182
wzhou2 <wzhou2@stuy.edu>
2020-10-26 20:10:43 -0400

    wrote add book and single test

--- add_book_main.asm
+++ add_book_main.asm
@@ -33,18 +33,35 @@ books:
 .align 2
 .byte 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
+
+.macro newline
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+.end_macro
+
+
 .text
 .globl main
 main:
-la $a0, books
-la $a1, isbn
-la $a2, title
-la $a3, author
-jal add_book
+	la $a0, books
+	la $a1, isbn
+	la $a2, title
+	la $a3, author
+	jal add_book
 
-# Write code to check the correctness of your code!
-li $v0, 10
-syscall
+	# Write code to check the correctness of your code!
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	newline
+	
+	move $a0, $v1
+	li $v0, 1
+	syscall 
+
+	li $v0, 10
+	syscall
 
 .include "hwk4.asm"
 
--- hwk4.asm
+++ hwk4.asm
@@ -137,10 +137,10 @@ get_book: # int, int get_book(Hashtable* books, string isbn)
     # a1 -> 13 ASCII characters -> s1
     # -> v0: index if found else -1 <- s2
     # -> v1: num of times strcmp is called <- s3
-    # Allocate space on stack for 5 registers (s0-5, ra)
+    # Allocate space on stack for 7 registers (s0-5, ra)
     # s4 = element size
     # s5 = capacity
-    addi $sp, $sp, -24
+    addi $sp, $sp, -28
     sw $s0, 0($sp)
     sw $s1, 4($sp)
     sw $s2, 8($sp)
@@ -186,7 +186,7 @@ get_book: # int, int get_book(Hashtable* books, string isbn)
         move $v0, $s2
         move $v1, $s3
 
-    # Deallocate space on the stack 5 registers (s0-4, ra)
+    # Deallocate space on the stack 7 registers (s0-5, ra)
     lw $s0, 0($sp)
     lw $s1, 4($sp)
     lw $s2, 8($sp)
@@ -194,11 +194,144 @@ get_book: # int, int get_book(Hashtable* books, string isbn)
     lw $s4, 16($sp)
     lw $s5, 20($sp)
     lw $ra, 24($sp)
-    addi $sp, $sp, 20
+    addi $sp, $sp, 28
 
     jr $ra 
 
-add_book:
+add_book: # int, int add_book(Hashtable* books, string isbn, string title, string author)
+    # a0 -> hashtable of books -> s0
+    # a1 -> 13 ASCII characters -> s1
+    # a2 -> title string -> s2
+    # a3 -> author -> s3
+    # -> v0 index of added <- s4
+    # -> v1 num of entries <- s5
+    # s6 = element size
+    # s7 = capacity
+
+    # check if books.size[4] == books.capacity[0]
+    lw $t0, 0($a0)
+    lw $t1, 4($a0)
+    bne $t0, $t1, add_book_not_full
+    li $v0, -1
+    li $v1, -1
+    jr $ra
+
+    add_book_not_full:
+        # Allocate space on stack for 9 registers (s0-7, ra)
+        addi $sp, $sp, -36
+        sw $s0, 0($sp)
+        sw $s1, 4($sp)
+        sw $s2, 8($sp)
+        sw $s3, 12($sp)
+        sw $s4, 16($sp)
+        sw $s5, 20($sp)
+        sw $s6, 24($sp)
+        sw $s7, 28($sp)
+        sw $ra, 32($sp)
+
+        move $s0, $a0
+        move $s1, $a1
+        move $s2, $a2
+        move $s3, $a3
+
+
+        jal get_book # get_book(books:a0, isbn:a1) -> v0: -1 (not found)
+        bgez $v0, add_book_done # v0 >= 0, book is found return v0 and v1
+        
+        # book not found
+        move $a0, $s0
+        move $a1, $s1
+        jal hash_book # hash_book(books:s0, isbn:s1) -> v0: index
+
+        move $s4, $v0 # starting index
+        li $s5, 0 # num of entries checked
+        lw $s6, 8($s0) # element size
+        lw $s7, 0($s0) # capacity
+
+        
+        add_book_find_empty_spot:
+            mul $a0, $s4, $s6 # offset
+            add $a0, $s0, $a0 # base+offset
+            addi $a0, $a0, 12 # 12 offset
+            addi $s5, $s5, 1 # increment
+
+            # check if ISBN (must start with '9') or empty/deleted
+            lbu $t0, 0($a0) # get char
+            li $t1, '9'
+            bne $t0, $t1, add_book_insert # empty/deleted -> add book here 
+            
+            # not char -> check next
+            # increment s4
+            addi $s4, $s4, 1 # check next index
+            seq $t0, $s4, $s5 # s4 = capacity -> $t0 === 1 else 0
+            mul $t0, $t0, $s5 # capacity if t0 === 1 else 0 
+            sub $s4, $s4, $t0 # s4 = s4 - t0 (to wrap around if s == capacity)
+
+            j add_book_find_empty_spot # do not have to worry about infinite loop because there will always be an empty spot 
+
+        add_book_insert:
+            # s6 = base address for the book struct to insert book in
+            move $s6, $a0
+
+            # update books.size:s0[4]++
+            lw $t0, 4($s0)
+            addi $t0, $t0, 1 # increment by 1
+            sw $t0, 4($s0)
+
+            # insert isbn
+            move $a1, $s1
+            li $a2, 13 # length of isbn 
+            jal memcpy # memcpy(bookStruct:a0/s6, isbn:s1, 13) -> v0: don't care
+            # increment s6 to title
+            addi $s6, $s6, 14 # isbn is 14 bytes (13+null terminator) 
+            # find length of title
+            li $a2, 0
+            li $t1, 24 # upper limit
+            add_book_find_title_length:
+                add $t0, $s6, $a2
+                lbu $t0, 0($t0)
+                beq $a2, $t1, add_book_insert_title
+                beqz $t0, add_book_insert_title 
+                addi $a2, $a2, 1 # increment
+                j add_book_find_title_length
+            add_book_insert_title: # insert title
+                move $a0, $s6
+                move $a1, $s2
+                jal memcpy # memcpy(bookStruct:s6, title:s2, title.length) -> v0: don't care
+            
+
+            addi $s6, $s6, 25 # title is 25 bytes (24+null terminator)
+            # find length of author
+            li $a2, 0
+            li $t1, 24 # upper limit
+            add_book_find_author_length:
+                add $t0, $s6, $a2
+                lbu $t0, 0($t0)
+                beq $a2, $t1, add_book_insert_author
+                beqz $t0, add_book_insert_author 
+                addi $a2, $a2, 1 # increment
+                j add_book_find_author_length
+            add_book_insert_author: # insert author
+                move $a0, $s6
+                move $a1, $s3
+                jal memcpy # memcpy(bookStruct:s6, author:s3, author.length) -> v0: don't care
+
+            move $v0, $s4 # copy s4, s5 to v0, v1
+            move $v1, $s5
+
+        add_book_done:
+        # Deallocate space on the stack 9 registers (s0-7, ra)
+        lw $s0, 0($sp)
+        lw $s1, 4($sp)
+        lw $s2, 8($sp)
+        lw $s3, 12($sp)
+        lw $s4, 16($sp)
+        lw $s5, 20($sp)
+        lw $s6, 24($sp)
+        lw $s7, 28($sp)
+        lw $ra, 32($sp)
+        addi $sp, $sp, 36
+
     jr $ra
 
 delete_book:
